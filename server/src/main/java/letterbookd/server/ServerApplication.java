1dc4a3866c2718dee6fd97a280eb6ae22f9d08bf
Leon <leon@embertechnology.nz>
2024-03-03 16:34:25 +1300

    Fixed up sql queries.

--- server/src/main/java/letterbookd/server/ServerApplication.java
+++ server/src/main/java/letterbookd/server/ServerApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
+	private static String url = "http://localhost:8080";
+	private static String startMsg = "Letterbookd server now running on ";
+
 
 	public static void main(String[] args) {
+		System.out.println(startMsg + url);
+
 		SpringApplication.run(ServerApplication.class, args);
 	}
-
 }
