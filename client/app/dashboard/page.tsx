d83d1e4a0c2214078bc48e427457dd4d874e8d27
Leon <leon@embertechnology.nz>
2024-09-14 13:46:32 +1200

    feat: Dashboard now retrieves and lists books #21

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,11 +1,41 @@
 "use client"
 
-function Dashboard() {
-	return (
-		<div>
-		dashboarrd
-		</div>
-	)
+import { useState, useEffect } from 'react';
+import notify from 'util/notify/notify';
+import api from 'util/api/api';
+
+export default function Dashboard() {
+	const [books, setBooks] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	async function getBooks() {
+		let books: object;
+		await api.books.getAllBooks()
+		.then((res) => books = res.data)
+		return books;
+	};
+
+	useEffect(() => {
+		getBooks()
+		.then((books) => {
+			setBooks(books);
+			setLoading(false);
+		})
+		.catch((err) => notify.info({message: err?.response?.data?.Message}));
+	});
+
+
+	if (loading) return <p>Loading...</p>
+		if (!books) return <p>No books available</p>
+
+			return (
+				<div>
+				<ul>
+				{books.map((book: any) => (
+					<li>{book.Title}</li>
+				))}
+				</ul>
+				</div>
+			)
 };
 
-export default Dashboard;
