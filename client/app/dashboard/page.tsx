68c820fc86c473e6065aa74012452af768c7f1d4
Leon <leon@embertechnology.nz>
2024-09-14 14:29:45 +1200

    feat: Dashboard book list styling #21

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,19 +1,21 @@
 "use client"
 
 import { useState, useEffect } from 'react';
+import { NextPageContext } from 'next';
 import notify from 'util/notify/notify';
 import api from 'util/api/api';
+import './dashboard.style.css';
+
+async function getBooks() {
+	let books: object;
+	await api.books.getAllBooks()
+	.then((res) => books = res.data)
+	return books;
+};
 
 export default function Dashboard() {
 	const [books, setBooks] = useState(null);
 	const [loading, setLoading] = useState(true);
-
-	async function getBooks() {
-		let books: object;
-		await api.books.getAllBooks()
-		.then((res) => books = res.data)
-		return books;
-	};
 
 	useEffect(() => {
 		getBooks()
@@ -21,21 +23,33 @@ export default function Dashboard() {
 			setBooks(books);
 			setLoading(false);
 		})
-		.catch((err) => notify.info({message: err?.response?.data?.Message}));
-	});
+		.catch((err) => notify.info({ message: err?.response?.data?.Message }));
+	}, []);
 
 
-	if (loading) return <p>Loading...</p>
-		if (!books) return <p>No books available</p>
+	if (loading) { 
+		return <p>Loading...</p>
+	}
+	if (!books) { 
+		return <p>No books available</p>
+	}
 
-			return (
-				<div>
-				<ul>
-				{books.map((book: any) => (
-					<li>{book.Title}</li>
-				))}
-				</ul>
-				</div>
-			)
+	return (
+		<div className='book-list-container'>
+		{books.map((book: any) => (
+			<div className='book-list-item'>
+			<div className='book-title'>
+			{book.Title}
+			</div>
+			<div className='book-author'>
+			{book.Author}
+			</div>
+			<div className='book-published-date'>
+			{new Date(book.Published).toLocaleDateString()}
+			</div>
+			</div>
+		))}
+		</div>
+	)
 };
 
