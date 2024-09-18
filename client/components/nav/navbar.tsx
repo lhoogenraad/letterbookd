57a11f4d4e7c8c0eecb814143686d61bad910e70
Leon <you@example.com>
2024-09-18 20:52:54 +1200

    feat: Navbar actually created

--- client/components/nav/navbar.tsx
+++ client/components/nav/navbar.tsx
@@ -1,7 +1,19 @@
 "use client"
 
+import "./navbar.css"
+
 export default function Navbar() {
-	<div className="">
-		
-	</div>
+	return (
+		<div className="navbar-container">
+			<div className="icon">
+				icon
+			</div>
+
+			<div className="navbar-btns">
+				<div>btn1</div>
+				<div>btn2</div>
+				<div>btn3</div>
+			</div>
+		</div>
+	);
 };
