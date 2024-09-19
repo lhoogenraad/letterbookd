a0aa46ae74458677da7da371cf92dca18bd654e9
Leon <you@example.com>
2024-09-20 11:14:04 +1200

    style: Some navbar stylin

--- client/components/nav/navbar.tsx
+++ client/components/nav/navbar.tsx
@@ -1,21 +1,13 @@
-"use client"
-
-import { usePathname } from 'next/navigation'
 import "./navbar.css"
 import hideNavbarRoutes from './routesToHideNavbar.tsx';
+import { IconBook } from '@tabler/icons-react';
 import LogoutButton from './logoutButton.tsx';
 
 export default function Navbar() {
-	const pathname = usePathname();
-
-	if (hideNavbarRoutes.includes(pathname)) {
-		return;
-	}
-
 	return (
 		<div className="navbar-container">
 			<div className="icon">
-				icon
+			<IconBook/>
 			</div>
 
 			<div className="navbar-title">
