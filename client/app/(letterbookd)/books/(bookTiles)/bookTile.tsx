5afc01f2526d5c892176bd242bceb4e2806adfe7
Leon <leon@embertechnology.nz>
2024-09-21 08:48:12 +1200

    fix: Fixed up img part of book tile

--- client/app/(letterbookd)/books/(bookTiles)/bookTile.tsx
+++ client/app/(letterbookd)/books/(bookTiles)/bookTile.tsx
@@ -1,10 +1,17 @@
 import './bookTile.css';
-import { Card } from '@mantine/core';
+import { Card, Image } from '@mantine/core';
 
 export default function BookTile({book}) {
 	return (
 		<Card className="book-tile-container">
-			<div className="book-tile-img">book img</div>
+			<Image 
+				className='book-tile-img'
+				src={null}
+				h={200}
+				w={200}
+				radius="md"
+				fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+			/>
 			<div className="book-tile-title">{book.Title}</div>
 			<div className="book-tile-author">{book.Author}</div>
 			<div className="book-tile-published">{new Date(book.Published).toLocaleDateString()}</div>
