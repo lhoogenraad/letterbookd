ac49e5f94aee2ff14a488a539d7e59b08ed32c55
Leon <you@example.com>
2024-09-30 19:49:41 +1300

    feat: Book info panel styled

--- client/components/books/bookInfoPanel.tsx
+++ client/components/books/bookInfoPanel.tsx
@@ -1,6 +1,52 @@
-export default function BookInfoPanel({book}) {
+import './bookInfoPanel.css';
+import { Image } from '@mantine/core';
+
+export default function BookInfoPanel({ book }) {
+	if (!book) {
+		return (
+			<div className="book-container">
+				<div>Rendering. . .</div>
+			</div>
+		)
+	}
 
 	return (
-		<div>{JSON.stringify(book)}</div>
+		<div className="book-container">
+			<div className="book-info-container">
+
+				<div className="book-info-item">
+					<span className="book-info-subheader">
+						Title
+					</span>
+					<h2 className="book-title">{book.Title}</h2>
+				</div>
+
+				<div className="book-info-item">
+					<span className="book-info-subheader">
+						Date Published
+					</span>
+					<div className="book-info-item-content">
+						{new Date(book.Published).toLocaleDateString()}
+					</div>
+				</div>
+
+				<div className="book-info-item">
+					<span className="book-info-subheader">
+						Synopsis
+					</span>
+					<div className="book-info-item-content">{book.Synopsis}</div>
+				</div>
+			</div>
+
+			<div className="book-info-img">
+				<Image 
+					className='book-tile-img'
+					src={null}
+					w='30vw'
+					radius="md"
+					fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+				/>
+			</div>
+		</div>
 	)
 }
