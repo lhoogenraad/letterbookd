aca166c37a6a46e76804a5092a833993a3156965
Leon <you@example.com>
2024-09-30 21:48:56 +1300

    style: Mega rizz on the review styling

--- client/app/(letterbookd)/books/(bookComponents)/bookReviewList/bookReview.tsx
+++ client/app/(letterbookd)/books/(bookComponents)/bookReviewList/bookReview.tsx
@@ -3,15 +3,17 @@ import { Rating } from '@mantine/core';
 
 export default function BookReview({review}) {
 	return (
-		<div className="review-container">
+		<div>
 			{/*JSON.stringify(review)*/}
 			<div className="review-header">{review.Title ?? 'PLACEHOLDER HEADER'}</div>
+			<div className="review-container">
 			<div className="review-metadata-container">
 				<div className="review-user">{ review.Username }</div>
 				<div className="vertical-seperator">|</div>
 				<Rating readOnly fractions={2} value={review.Rating/2} className="review-value"/>
 				</div>
 			<div className="review-description">{review.Description}</div>
+			</div>
 		</div>
 	)
 }
