d195f6abbe69bfa9f5b6eafb266618c4d35c05a9
Leon <you@example.com>
2024-10-02 18:55:03 +1300

    chore: Added comment to api method

--- client/util/api/retrievers/reviews.ts
+++ client/util/api/retrievers/reviews.ts
@@ -7,6 +7,10 @@ const getBookReviews = async (bookId: number) => {
 	return await api.get(`/books/${bookId}/reviews`).then((res) => res);
 };
 
+/**
+ * Retrieve summary of given book's rating(s)
+ * Includes avg rating and number of reviews
+ */
 const getBookAverageAndNumberReviews = async (bookId: number) => {
 	return await api.get(`/books/${bookId}/reviews/summary`).then((res) => res);
 }
