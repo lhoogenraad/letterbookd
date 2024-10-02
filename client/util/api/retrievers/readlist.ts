beda6e89a68516f746719387bba990f74d551d55
Leon <you@example.com>
2024-10-02 20:25:59 +1300

    feat: Add book to readlist button connected

--- client/util/api/retrievers/readlist.ts
+++ client/util/api/retrievers/readlist.ts
@@ -4,14 +4,17 @@ import api from '../axios';
  * Retrieve readlist for curr user
  */
 const getReadList = async () => {
-	return await api.get(`/api/read-list`).then((res) => res);
+	return await api.get(`/read-list`).then((res) => res);
 }
 
 /**
  * Add book to read list
  */
-const addBookToReadList = async (bookId: number) => {
-	return await api.post(`/api/read-list/book/${bookId}`).then((res) => res);
+const addBookToReadList = async (bookId: number, status: string) => {
+	return await api.post(
+		`/read-list/book/${bookId}`,
+		{status}
+	).then((res) => res);
 }
 
 /**
@@ -19,7 +22,7 @@ const addBookToReadList = async (bookId: number) => {
  */
 const updateReadListItem = async (bookId: number, status: string) => {
 	return await api.put(
-		`/api/read-list/book/${bookId}`,
+		`/read-list/book/${bookId}`,
 		{status}
 	).then((res) => res);
 }
