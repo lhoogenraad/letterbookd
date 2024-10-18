e8de2b5a64f9830d3b683d2d764090f4ff5c8f22
Leon <you@example.com>
2024-10-18 18:28:23 +1300

    bfV time

--- client/app/(letterbookd)/read-list/(readlistItem)/readlistItem.tsx
+++ client/app/(letterbookd)/read-list/(readlistItem)/readlistItem.tsx
@@ -1,5 +1,25 @@
-
+import './readlistItem.css';
+import { Image } from '@mantine/core';
 
 export default function ReadListItem({item}) {
-	return <div>{item.BookName}</div>
+	return (
+		<div className="readlist-item-container">
+			<div className="readlist-header-container">
+				<div className="img">
+					<Image 
+						src={null}
+						h='auto'
+						w='auto'
+						radius="md"
+						fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+					/>
+				</div>
+				<div className="title">{item.BookName}</div>
+			</div>
+			<div className="readlist-item-body-container">
+				<div className=""></div>
+				<div className=""></div>
+			</div>
+		</div>
+	)
 }
