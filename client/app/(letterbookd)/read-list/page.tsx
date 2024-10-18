65b9c994b6f161ed4658f3ba44c6406dd1cbff0c
Leon <you@example.com>
2024-10-18 17:37:46 +1300

    feat: JSON strungified read list is loaded

---
+++ client/app/(letterbookd)/read-list/page.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import { useState, useEffect } from 'react';
+import '@mantine/core/styles.css';
+import notify from 'util/notify/notify';
+import api from 'util/api/api';
+import './readlist.css';
+
+async function getReadList() {
+	let books: object;
+	await api.readlist.getReadList()
+		.then((res) => books = res.data)
+	return books;
+};
+
+export default function Dashboard() {
+	const [readList, setReadList] = useState(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		// Get books
+		getReadList()
+			.then((list) => {
+				setReadList(list);
+				setLoading(false);
+			})
+			.catch((err) => notify.error({ message: err?.response?.data?.Message }));
+	}, []);
+
+
+	if (loading) {
+		return <p>Loading...</p>
+	}
+	if (!readList || !readList.length) {
+		return <p>It appears you've got no books on your read list!</p>
+	}
+
+	return (
+		<div className='readlist-container'>
+		{JSON.stringify(readList)}
+		</div>
+	)
+};
+
