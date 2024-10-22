3e0db071605d7717fc37c194aa575309dbafe2ba
Leon <you@example.com>
2024-10-22 21:51:30 +1300

    feat: Added more stats for given book.

--- client/app/(letterbookd)/books/(bookComponents)/bookInfoPanel/bookReviewAverage.tsx
+++ client/app/(letterbookd)/books/(bookComponents)/bookInfoPanel/bookReviewAverage.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { IconBooks, IconRosetteDiscountCheckFilled } from '@tabler/icons-react';
 import { Rating, Tooltip } from '@mantine/core';
 import { useState, useEffect } from 'react';
 import api from 'util/api/api';
@@ -7,16 +8,13 @@ import notify from 'util/notify/notify';
 
 export default function BookReviewAverage({ bookId }) {
 	const [loading, setLoading] = useState(false);
-	const [avgRating, setAvgRating] = useState(0);
-	const [numReviews, setNumReviews] = useState(0);
+	const [stats, setStats] = useState({});
 
 	const loadBookReviewSummary = (bookId: number) => {
 		setLoading(true);
 		api.reviews.getBookAverageAndNumberReviews(bookId)
 			.then((res) => {
-				const { avgRating, numReviews } = res.data;
-				setAvgRating(avgRating);
-				setNumReviews(numReviews);
+				setStats(res.data)
 			})
 			.catch((err) => {
 				console.error(err);
@@ -38,16 +36,31 @@ export default function BookReviewAverage({ bookId }) {
 			<div className="book-info-review-aggregate">
 				<div className="review-average-rating">
 					Average rating:
-					<Tooltip label={avgRating / 2}>
-						<Rating readOnly fractions={2} value={avgRating / 2} />
+					<Tooltip label={stats.avgRating / 2}>
+						<Rating readOnly fractions={2} value={stats.avgRating / 2} />
 					</Tooltip>
 				</div>
 				<div className="review-number-views">
 					{
-						numReviews == 0 ? 
-						`No reviews yet.` :
-						`(Out of ${numReviews} reviews)`
+						stats.numReviews == 0 ?
+							`No reviews yet.` :
+							`(Out of ${stats.numReviews} reviews)`
 					}
+				</div>
+				<div className="readlist-stats">
+					<Tooltip label={"Number of completed reads"}>
+						<div className="readlist-stats-item">
+							<IconRosetteDiscountCheckFilled />
+							<div>{stats.numCompletedReads}</div>
+						</div>
+					</Tooltip>
+
+					<Tooltip label={"Number of times this book was added to someone's readlist"}>
+						<div className="readlist-stats-item">
+							<IconBooks />
+							<div>{stats.numReadlistOccurences}</div>
+						</div>
+					</Tooltip>
 				</div>
 			</div>
 		</div>
