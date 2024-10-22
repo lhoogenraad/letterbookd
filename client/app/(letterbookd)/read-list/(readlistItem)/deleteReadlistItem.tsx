6b0ac6c3eccc29c6b062342eddd7c68f3ba92ae0
Leon <you@example.com>
2024-10-22 17:45:13 +1300

    feat: Uhh state management go brrr

---
+++ client/app/(letterbookd)/read-list/(readlistItem)/deleteReadlistItem.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { Button } from '@mantine/core';
+import { useState } from 'react';
+import api from 'util/api/api';
+import notify from 'util/notify/notify';
+
+export default function DeleteReadlistItem({ bookId, closeModal, removeItem }) {
+	const [loading, setLoading] = useState(false);
+
+	const deleteReadlistItem = async () => {
+		setLoading(true);
+		await api.readlist.removeReadlistItem(bookId)
+			.then(() => {
+				closeModal();
+				removeItem(bookId);
+			})
+			.catch((err) => notify.error({ message: err.response.data.Message }))
+			.finally(() => setLoading(false))
+	}
+
+	return (
+		<div style={{
+			display: 'flex',
+			flexDirection: 'row',
+			justifyContent: 'space-around',
+			maxWidth: '500px',
+			margin: '0 auto',
+		}}>
+			<Button onClick={() => closeModal()} variant="subtle" color="blue">No, go back!</Button>
+			<Button onClick={() => deleteReadlistItem()} loading={loading} variant="subtle" color="red">Yes please</Button>
+		</div>
+	)
+}
