84c4d55e39231d859af4671db3e3b5d3203e95e3
Leon <you@example.com>
2024-11-01 17:53:35 +1300

    update book URL method

--- scripts/books/covers/upload.go
+++ scripts/books/covers/upload.go
@@ -3,6 +3,7 @@ package covers
 import (
 	"fmt"
 	"scripts/books"
+	"scripts/util"
 )
 
 func AddCoversToBooks() error {
@@ -12,4 +13,10 @@ func AddCoversToBooks() error {
 	if err != nil {return err}
 	fmt.Println(books)
 	return nil
+}
+
+func SetBookUrl(bookId int, coverURL string) error {
+	updateQuery := `UPDATE books SET cover_url = ? WHERE id = ?`
+	_, err := util.DB.Exec(updateQuery, coverURL, bookId)
+	return err
 }
