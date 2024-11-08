c6270d98b76171c6eef7f54bdadd420844f798c5
Leon <you@example.com>
2024-11-08 17:25:44 +1300

    loading prop put on search form prop correctly

--- client/components/openlibrary/openLibrary.tsx
+++ client/components/openlibrary/openLibrary.tsx
@@ -2,14 +2,22 @@
 
 import OpenLibrarySearchForm from "./openLibrarySearch";
 import api from "../../util/api/api";
+import { useState } from "react";
 
 export default function OpenLibrary(){
+	const [loading, setLoading] = useState(false);
+
 	const submit = async (values:any) => {
-		const res = await api.openlibrary.searchForBookOL(values);
-		console.log(res.data);
+		setLoading(true);
+		try {
+			const res = await api.openlibrary.searchForBookOL(values);
+			console.log(res.data);
+		} finally {
+			setLoading(false);
+		}
 	}
 
 	return (
-		<OpenLibrarySearchForm submitSearch={submit} loading={false} />
+		<OpenLibrarySearchForm submitSearch={submit} loading={loading} />
 	)
 }
