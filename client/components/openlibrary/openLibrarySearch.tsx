b45d602d070ddb0d9e529ff414e0d79527f42294
Leon <you@example.com>
2024-11-08 14:31:58 +1300

    feat: Formatting code

--- client/components/openlibrary/openLibrarySearch.tsx
+++ client/components/openlibrary/openLibrarySearch.tsx
@@ -4,7 +4,7 @@ import { Button, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 
 
-export default function OpenLibrarySearch ({submitSearch, loading}: {submitSearch: Function, loading: boolean}) {
+export default function OpenLibrarySearch({ submitSearch, loading }: { submitSearch: Function, loading: boolean }) {
 	const form = useForm({
 		mode: 'uncontrolled',
 		initialValues: {
@@ -15,32 +15,32 @@ export default function OpenLibrarySearch ({submitSearch, loading}: {submitSearc
 	});
 
 	const formSubmit = (values: any) => {
-		const {title, author, publisher} = values;
-		submitSearch({title, author, publisher})
+		const { title, author, publisher } = values;
+		submitSearch({ title, author, publisher })
 	}
 
 	return (
 		<div>
 			<form onSubmit={formSubmit}>
-			<TextInput
-				label="Book title"
-				placeholder="I, claudius..."
-				key={form.key("title")}
-				{...form.getInputProps("title")}
-			/>
-			<TextInput
-				label="Author name"
-				placeholder="Robet Graves"
-				key={form.key("author")}
-				{...form.getInputProps("author")}
-			/>
-			<TextInput
-				label="Publisher"
-				placeholder="Penguin books"
-				key={form.key("publisher")}
-				{...form.getInputProps("publisher")}
-			/>
-			<Button loading={loading} disabled={loading} type="submit">Submit</Button>
+				<TextInput
+					label="Book title"
+					placeholder="I, claudius..."
+					key={form.key("title")}
+					{...form.getInputProps("title")}
+				/>
+				<TextInput
+					label="Author name"
+					placeholder="Robet Graves"
+					key={form.key("author")}
+					{...form.getInputProps("author")}
+				/>
+				<TextInput
+					label="Publisher"
+					placeholder="Penguin books"
+					key={form.key("publisher")}
+					{...form.getInputProps("publisher")}
+				/>
+				<Button loading={loading} disabled={loading} type="submit">Submit</Button>
 			</form>
 		</div>
 	)
