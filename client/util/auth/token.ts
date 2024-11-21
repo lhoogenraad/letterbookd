d7d66c1b838bbad62c9b20c4d3b7ad1dafe87aa1
Leon <you@example.com>
2024-11-21 21:09:14 +1300

    feat: Clear token on logout

--- client/util/auth/token.ts
+++ client/util/auth/token.ts
@@ -4,6 +4,11 @@ const saveToken = (token: string) => {
 	cookie.set('authToken', token, { expires: 1, secure: true, sameSite: 'strict' });
 }
 
+const deleteCookie = (cookieName: string) => {
+	cookie.remove(cookieName, {path: ''});
+}
+
 export default {
 	saveToken,
+	deleteCookie,
 }
