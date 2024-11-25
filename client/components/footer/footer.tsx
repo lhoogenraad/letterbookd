e5cb3f24858259bf60485dc4e116491a0680e853
Leon <you@example.com>
2024-11-25 17:53:09 +1300

    feat: Social links sussed out

--- client/components/footer/footer.tsx
+++ client/components/footer/footer.tsx
@@ -1,10 +1,33 @@
+"use client";
+
+import Link from 'next/link';
 import './footer.css';
+import { EmailIcon, GithubIcon, LinkedinIcon } from "next-share";
+import notify from 'util/notify/notify';
 
 export default function Footer() {
+	const leonEmail = `le.o.n@outlook.com`;
+
+	const copyEmail = () => {
+		navigator.clipboard.writeText(leonEmail);
+		notify.info({message: `Copied Leon's email to clipboard!`});
+	}
+
 	return (
 		<div className="footer-container">
 			<div className="primary-content">
-				This application was made as a passion project by @Leon Hoogenraad
+				This application was made as a passion project by Leon Hoogenraad
+				<div className="social-links">
+					<Link href="https://www.linkedin.com/in/leon-hoogenraad-444b521ab">
+						<LinkedinIcon size={26} round />
+					</Link>
+					<Link href="https://github.com/lhoogenraad">
+						<GithubIcon size={26} round />
+					</Link>
+					<div onClick={copyEmail} className="copy-button">
+						<EmailIcon size={26} round />
+					</div>
+				</div>
 			</div>
 			<div className="secondary-content">
 				©️ Letterbookd
