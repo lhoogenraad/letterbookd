c28fad93f7173976fa8e8ba672781ac25d7bd50e
Leon <you@example.com>
2024-11-25 16:48:05 +1300

    title is now nav link

--- client/app/(letterbookd)/books/(bookComponents)/bookReviewList/bookReview.tsx
+++ client/app/(letterbookd)/books/(bookComponents)/bookReviewList/bookReview.tsx
@@ -7,11 +7,16 @@ import ReviewHeart from 'components/reviews/likes/reviewHeart';
 export default function BookReview({ review, bookId, showTitle}) {
 	return (
 		<div>
+		<Link 
+		style={{ textDecoration: "inherit", color: "inherit" }} 
+		href={`/books/${bookId}/reviews/${review.Id}`}
+		>
 			{
 				showTitle ? <div className="review-header">{review.BookTitle ?? 'Review'}</div>
 				:
-				<div className="review-header"></div>
+				<div className="review-header">{review.BookTitle ?? 'Review'}</div>
 			}
+			</Link>
 			<div className="review-container">
 				<div className="review-metadata-container">
 					<div className="review-user">{review.Username}</div>
