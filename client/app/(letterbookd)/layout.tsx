fb92c87c2f99deee17c076f406eaaa3bee47f31c
Leon <you@example.com>
2024-11-25 17:35:49 +1300

    feat: Made footer

--- client/app/(letterbookd)/layout.tsx
+++ client/app/(letterbookd)/layout.tsx
@@ -1,14 +1,20 @@
 import '@mantine/carousel/styles.css';
-import Navbar from '../../components/nav/navbar';
+import Navbar from 'components/nav/navbar';
+import Footer from 'components/footer/footer';
 import '@mantine/notifications/styles.css';
 import './app.style.css';
 
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
 	return (
-		<div>
-			<Navbar />
-			<div className='app'>
-				{children}
+		<div className="app-container">
+			<div className="upper-content">
+				<Navbar />
+				<div className='app'>
+					{children}
+				</div>
+			</div>
+			<div className="lower-content">
+				<Footer />
 			</div>
 		</div>
 	)
---
+++ client/components/footer/footer.tsx
@@ -0,0 +1,14 @@
+import './footer.css';
+
+export default function Footer() {
+	return (
+		<div className="footer-container">
+			<div className="primary-content">
+				This application was made as a passion project by @Leon Hoogenraad
+			</div>
+			<div className="secondary-content">
+				©️ Letterbookd
+			</div>
+		</div>
+	);
+};
